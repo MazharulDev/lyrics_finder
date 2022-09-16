519657f74dd23032ddbea29f21d46140dfe3449a
Md Mazharul Islam <mazharulislam.dev@gmail.com>
2022-09-16 19:27:59 +0600

    lyrics show link added

--- src/components/LyricsList/LyricsList.tsx
+++ src/components/LyricsList/LyricsList.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, Grid, Typography } from '@mui/material';
+import { Button, Card, CardActions, CardContent, Grid, Link, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import React, { useEffect, useState } from 'react';
 import LyricsType from '../../types/models';
@@ -9,7 +9,7 @@ const LyricsList = () => {
     const [Lyrics, setLyrics] = useState<LyricsType[]>([]);
     console.log(Lyrics);
     useEffect(() => {
-        fetch(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=237d5d86270eff9d001af5d3e4a2364b`)
+        fetch(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=20&country=us&f_has_lyrics=1&apikey=237d5d86270eff9d001af5d3e4a2364b`)
             .then(res => res.json())
             .then(data => setLyrics(data.message.body.track_list))
     }, [])
@@ -26,6 +26,12 @@ const LyricsList = () => {
                                     <Typography gutterBottom>
                                         <Typography variant='h6' color="blueviolet">{lyric.track.track_name} </Typography> <Typography>By</Typography> <Typography color="text.seceondary">{lyric.track.artist_name}</Typography>
                                     </Typography>
+                                    <Typography>Album Name: {lyric.track.album_name}</Typography>
+                                    <CardActions>
+                                        <Link href={lyric.track.track_share_url} underline="hover">
+                                            {"Show Lyrics"}
+                                        </Link>
+                                    </CardActions>
                                 </CardContent>
                             </Card>
                         </Box>
