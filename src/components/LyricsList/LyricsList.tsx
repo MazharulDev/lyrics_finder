c6310c19089fa40824665b7db8a5c3b1078c160e
Md Mazharul Islam <mazharulislam.dev@gmail.com>
2022-09-16 12:40:11 +0600

    navbar added

---
+++ src/components/LyricsList/LyricsList.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const LyricsList = () => {
+    const [Lyrics, setLyrics] = useState()
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default LyricsList;
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+
+
+
+
+const Navbar = () => {
+    return (
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component="a"
+                        href=""
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'flex', justifyContent: "center" },
+                            flexGrow: 1,
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        Lyrics Finder
+                    </Typography>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+};
+
+export default Navbar;
